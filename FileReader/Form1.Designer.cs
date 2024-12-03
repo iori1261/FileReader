7c0ed1fc338935976af223b6d560a0116d3001b7
236137 <236137@KCSF0971>
2024-12-03 09:23:29 +0900

    プロジェクト ファイルを追加します。

---
+++ FileReader/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FileReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.btnRead.Location = new System.Drawing.Point(39, 281);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(181, 53);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "読み込み";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.btnWrite.Location = new System.Drawing.Point(39, 358);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(181, 53);
+            this.btnWrite.TabIndex = 1;
+            this.btnWrite.Text = "書き込み";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(264, 59);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(524, 379);
+            this.txtText.TabIndex = 2;
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lblFilePath.Location = new System.Drawing.Point(12, 21);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(767, 27);
+            this.lblFilePath.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label lblFilePath;
+    }
+}
+
---
+++ FileReader/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FileReader
+{
+    public partial class Form1 : Form
+    {
+        String filePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //ファイルの存在をチェック
+            //File.Exists(チェックしたいファイルのパス)
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            //タイトルの設定
+            fileDialog.Title = "ファイルをしてください";
+            //フィルターの設定(テキストファイルのみすべてのファイル)
+            fileDialog.Filter = "Text files (*.txt)|*.txt|All files(*.*)|*.*";
+            //ダイアログを表示(ファイルの選択された)
+            if(fileDialog.ShowDialog() == DialogResult.OK) 
+            {
+                filePath = fileDialog.FileName; 
+            }
+
+            //ファイルのパス情報を表示する
+            lblFilePath.Text = fileDialog.FileName;
+
+            if (File.Exists(filePath))
+            {
+                //ファイルの読み込みを行う
+                StreamReader sr = new StreamReader(filePath);
+
+                //全業を読み込んでtxttextに表示する
+                txtText.Text = sr.ReadToEnd();
+
+                //StreamReaderをクローズする
+                sr.Close();
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            //テキストボックスの内容をファイルに保存する。
+            //①書き込み前にファイルの存在チェック
+            if (File.Exists(filePath))
+            {
+                //②対象ファイルをオープン 第2引数 true:追記 false:上書き
+                StreamWriter sw = new StreamWriter(filePath,false);
+                //③対象ファイルに内容を保存
+                sw.Write(txtText.Text);
+                //④使用後はクローズ
+                sw.Close();
+
+            }
+        }
+    }
+}
